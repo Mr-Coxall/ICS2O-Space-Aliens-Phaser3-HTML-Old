53c8bf9a547e1205e5224dd56c10fcd32f9187a5
Patrick Coxall <mr.coxall@mths.ca>
2022-04-25 18:02:02 -0400

    added image to splashScene

--- js/splashScene.js
+++ js/splashScene.js
@@ -15,6 +15,8 @@ class SplashScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "splashScene" })
+
+    this.splashSceneBackgroundImage = null
   }
 
   /**
@@ -24,7 +26,7 @@ class SplashScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("ffffff")
+    this.cameras.main.setBackgroundColor("#ffffff")
   }
 
   /**
@@ -33,6 +35,7 @@ class SplashScene extends Phaser.Scene {
    */
   preload() {
     console.log("Splash Scene")
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
   }
 
   /**
@@ -41,7 +44,13 @@ class SplashScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    // pass
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    )
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
   /**
@@ -51,7 +60,9 @@ class SplashScene extends Phaser.Scene {
    *  @param {number} delta - The delta time in ms since the last frame.
    */
   update(time, delta) {
-    this.scene.switch("titleScene")
+    if (time > 3000) {
+      this.scene.switch("titleScene")
+    }
   }
 }
 
