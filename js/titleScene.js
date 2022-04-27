79872488646df9259f79b0aa3510aabc10e20dbc
Patrick Coxall <mr.coxall@mths.ca>
2022-04-26 21:41:37 -0400

    lesson-06

--- js/titleScene.js
+++ js/titleScene.js
@@ -15,6 +15,14 @@ class TitleScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "titleScene" })
+
+    this.titleSceneBackgroundImage = null
+    this.titleSceneText = null
+    this.titleSceneTextStyle = {
+      font: "200px Times",
+      fill: "#fde4b9",
+      align: "center",
+    }
   }
 
   /**
@@ -33,6 +41,7 @@ class TitleScene extends Phaser.Scene {
    */
   preload() {
     console.log("Title Scene")
+    this.load.image("titleSceneBackground", "assets/aliens_screen_image.jpg")
   }
 
   /**
@@ -41,7 +50,15 @@ class TitleScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    // pass
+    this.titleSceneBackgroundImage = this.add
+      .sprite(0, 0, "titleSceneBackground")
+      .setScale(2.75)
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1080 / 2
+
+    this.titleSceneText = this.add
+      .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
+      .setOrigin(0.5)
   }
 
   /**
