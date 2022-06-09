327117b4337c45181a9ff8b4c76e1176aae8429f
Patrick Coxall <mr.coxall@mths.ca>
2022-06-09 19:35:01 -0400

    update to Lesson-07

--- js/menuScene.js
+++ js/menuScene.js
@@ -15,6 +15,9 @@ class MenuScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "menuScene" })
+
+    this.menuSceneBackgroundImage = null
+    this.startButton = null
   }
 
   /**
@@ -33,6 +36,8 @@ class MenuScene extends Phaser.Scene {
    */
   preload() {
     console.log("Menu Scene")
+    this.load.image("menuSceneBackground", "./assets/aliens_screen_image2.jpg")
+    this.load.image("startButton", "./assets/start.png")
   }
 
   /**
@@ -41,7 +46,13 @@ class MenuScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    // pass
+    this.menuSceneBackgroundImage = this.add.sprite(0, 0, "menuSceneBackground")
+    this.menuSceneBackgroundImage.x = 1920 / 2
+    this.menuSceneBackgroundImage.y = 1080 / 2
+
+    this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, "startButton")
+    this.startButton.setInteractive({ useHandCursor: true })
+    this.startButton.on("pointerdown", () => this.clickButton())
   }
 
   /**
@@ -52,6 +63,10 @@ class MenuScene extends Phaser.Scene {
    */
   update(time, delta) {
     // pass
+  }
+
+  clickButton() {
+    this.scene.start("gameScene")
   }
 }
 
